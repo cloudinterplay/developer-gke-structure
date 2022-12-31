72d7e876303aac21ddb58134f32c933f61f311fb
Maxim Generalov <generalov.maksim@gmail.com>
2022-12-31 21:58:41 +0000

    update2

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -2,45 +2,14 @@ module "gke" {
   depends_on = [
     google_compute_subnetwork.subnetwork
   ]
-  source      = "../../../tf-modules-gcp/terraform-gcp-gke-cluster/modules/cluster"
-  project     = var.gcp.project_id
-  cluster     = var.gke.cluster
-  node_pools  = var.gke.node_pools
-  network                  = google_compute_network.vpc_network.id
-  subnetwork               = google_compute_subnetwork.subnetwork.id
-  private_cluster_config   = var.gke.private_cluster_config
-
-  node_pools_oauth_scopes = {
-    all = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
-  }
-  node_pools_labels = {
-    all = {}
-
-    default-node-pool = {
-      default-node-pool = true
-    }
-  }
-
-  node_pools_taints = {
-    all = []
-
-    default-node-pool = [
-      {
-        key    = "default-node-pool"
-        value  = true
-        effect = "PREFER_NO_SCHEDULE"
-      },
-    ]
-  }
-
-  node_pools_tags = {
-    all = []
-
-    default-node-pool = [
-      "default-node-pool",
-    ]
-  }
+  source  = "../../../tf-modules-gcp/terraform-gcp-gke-cluster/modules/cluster"
+  cluster = merge(
+    {
+      project    = var.gcp.project_id,
+      network    = google_compute_network.vpc_network.id,
+      subnetwork = google_compute_subnetwork.subnetwork.id
+    },
+    var.gke.cluster
+  )
+  node_pools = var.gke.node_pools
 }
